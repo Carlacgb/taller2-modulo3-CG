c53e12439a506f6c9e6043711219d6b7c1ba74e0
Caita <carlacgb@gmail.com>
2024-11-18 22:01:30 -0300

    prompts en JavaScrip

---
+++ assets/js/app.js
@@ -0,0 +1,8 @@
+const nombre = prompt("¿Cuál es tu nombre?", "")
+console.log(nombre)
+alert("Hola " + nombre)
+
+const email = prompt("¿Cuál es tu correo electrónico", "")
+console.log(email)
+//validación de correo electrónico que tenga una @ o si tipo es string
+alert("Tu correo ingresado es " + email)
